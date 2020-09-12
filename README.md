dca8c58043c4752096bbdad9cd5f26be59a53c0f
arthurah <thebestarthure@hotmail.com>
2020-09-12 14:31:24 +0300

    Add sound

--- README.md
+++ README.md
@@ -5,5 +5,6 @@
     
 <h1 align="center">   batman2600 </h1>
 <p align="center">
-  Unofficial<em> Dark knight <img src="https://github.com/buildkite/emojis/blob/master/img-buildkite-64/batman.png" width="20" height="20" alt="batman"/></em> game for Atari 2600
+  Unofficial<em> Dark knight <img src="https://github.com/buildkite/emojis/blob/master/img-buildkite-64/batman.png" width="20" height="20" alt="batman"/></em> game for Atari 2600 <br>
+<br><a href="https://gifyu.com/image/4pQr"><img src="https://s8.gifyu.com/images/gotham2600.png" alt="gotham2600.png" width="680" border="0" /></a>
 </p>
--- kernel.asm
+++ kernel.asm
@@ -9,11 +9,20 @@
 P0PosX         byte        
 P0PosY         byte        
 P1PosX         byte        
-P1PosY         byte        
+P1PosY         byte   
+
 BatSpritePtr   word        ; pointer to p0 sprite table
 BatColorPtr    word        ; pointer to p0 color lookup table
 JSpritePtr     word        ; pointer to p1 sprite lookup table
 JColorPtr      word        ; pointer to p1 color lookup table
+
+;TODO
+;anim, scrolling, enemies,score
+
+; for scoreboard
+Second ds
+Minute ds
+FontBuf ds
 
 ; constants
 BAT_HEIGHT = #$10          	     
@@ -53,6 +62,10 @@ Init
     sta P1PosY
     lda #54
     sta P1PosX       
+    
+    lda #00
+    sta Temp
+    
 InitPointers
 
     lda #<BatSprite
@@ -73,12 +86,64 @@ InitPointers
     lda #<JColor
     sta JColorPtr       
     lda #>JColor
-    sta JColorPtr+1     
+    sta JColorPtr+1
+    
+    jmp StartNewFrame
+    ;channel 0
+
+; Make sound subroutines
+
+;pass a, x ,y
+MakeSoundC0
+  sta AUDV0
+  stx AUDF0
+  sty AUDC0 
+  rts
+
+    ;channel 1
+MakeSoundC1
+  sta AUDV1
+  stx AUDF1
+  sty AUDC1 
+  rts
+  
+GetBCDBitmap subroutine
+; First fetch the bytes for the 1st digit
+  pha
+  and #$0F 
+  sta Temp 
+  asl
+  asl
+  adc Temp 
+  tay
+  lda #5 
+  sta Temp
+; save original BCD number
+; mask out the least significant digit
+; multiply by 5 ;->Y
+; count down from 5
+.loop1
+  lda DigitsBitmap,y
+  and #$0F
+  sta FontBuf,x 
+  iny
+  inx
+  dec Temp
+  bne .loop1
+; Now do the 2nd digit
+        pla
+        lsr
+        lsr
+        lsr
+; mask out leftmost digit
+; store leftmost digit
+; restore original BCD number
+ rts
 
 StartNewFrame
   lda Counter+1
-  sta Counter
-
+  sta Counter  
+  
 Vsync
   lda #2 
   sta VSYNC ;3
@@ -100,22 +165,9 @@ Vsync
   lda #$00
   sta Temp 
 
- ; ldx #150
- jmp Scroll_End
-Scroll
-  lsr #Screen_PF2+1,X   ; Scroll Line X-1 (= 3-0)
-  rol #Screen_PF1+1,X
-  ror #Screen_PF0+1,X
-  lda #Screen_PF0+1,X
-  and #%00001000
-  beq Scroll_1   
-  lda #Screen_PF2+1,X            
-  ora #%10000000      
-  sta #Screen_PF2+1,X
-Scroll_1               
-  dex
-  bne Scroll
-Scroll_End         
+  ldx #30
+  ;jmp Scroll_End
+  
   ;lda #1
   ;cmp IsJumping
 
@@ -141,6 +193,23 @@ CheckJumping
   dec P0PosY   
   lda #1
   sta IsFalling
+  
+  
+Scroll
+  lsr #Screen_PF2-1,X   ; Scroll Line X-1 (= 3-0)
+  rol #Screen_PF1-1,X
+  ror #Screen_PF0-1,X
+  lda #Screen_PF0-1,X
+  and #%00001000
+  beq Scroll_1   
+  lda #Screen_PF2-1,X            
+  ora #%10000000      
+  sta #Screen_PF2-1,X
+Scroll_1               
+  dex
+  bne Scroll
+Scroll_End           
+  
   
   ;; END  ;;
   
@@ -217,7 +286,7 @@ GameVisibleLine
     lda #0                   ; else, set lookup index to 0
 .DrawSpriteP0:
    clc                       ; clear carry flag 
-  ; adc AnimOffset           ; jump to sprite frame 
+  ;adc AnimOffset           ; jump to sprite frame 
    tay                       ; load Y so we can work with pointer
 
    lda (BatSpritePtr),Y      ; load player bitmap slice of data
@@ -241,11 +310,17 @@ Overscan:
 
   lda #0
   sta CTRLPF
-  lda #$00
+  lda #$01
   sta COLUPF
 
   lda #$A0
   sta COLUBK
+
+  ; silent
+  lda #0
+  sta AUDV0
+  sta AUDF0
+  sta AUDC0
 
   REPEAT 30
     sta WSYNC            
@@ -262,10 +337,19 @@ CheckP0Up:
   lda #%00010000
   bit SWCHA
   bne CheckP0Down
+ 
+  ; make sound 
+  lda #1
+  sta AUDV0
+  sta AUDF0
+  sta AUDC0
+  
   lda #1
   cmp IsFalling
+  
   beq CheckP0Down
-  sta IsJumping
+  sta IsJumping  
+  
   ;inc P0PosY 
   ;; write here
 
@@ -416,27 +500,27 @@ Screen_PF0
       .byte #%11110000	; Scanline 121
       .byte #%11110000	; Scanline 120
       .byte #%11110000	; Scanline 119
-      .byte #%11110000	; Scanline 118
-      .byte #%11110000	; Scanline 117
-      .byte #%11110000	; Scanline 116
-      .byte #%11110000	; Scanline 115
-      .byte #%11110000	; Scanline 114
-      .byte #%11110000	; Scanline 113
-      .byte #%11110000	; Scanline 112
-      .byte #%11110000	; Scanline 111
-      .byte #%11110000	; Scanline 110
-      .byte #%11110000	; Scanline 109
-      .byte #%11110000	; Scanline 108
-      .byte #%01110000	; Scanline 107
-      .byte #%01110000	; Scanline 106
-      .byte #%01010000	; Scanline 105
-      .byte #%01010000	; Scanline 104
-      .byte #%01010000	; Scanline 103
-      .byte #%01010000	; Scanline 102
-      .byte #%01010000	; Scanline 101
-      .byte #%01010000	; Scanline 100
-      .byte #%01010000	; Scanline 99
-      .byte #%01000000	; Scanline 98
+      .byte #%00110000	; Scanline 118
+      .byte #%00110000	; Scanline 117
+      .byte #%00110000	; Scanline 116
+      .byte #%00110000	; Scanline 115
+      .byte #%00110000	; Scanline 114
+      .byte #%00110000	; Scanline 113
+      .byte #%00110000	; Scanline 112
+      .byte #%00000000	; Scanline 111
+      .byte #%00000000	; Scanline 110
+      .byte #%00000000	; Scanline 109
+      .byte #%00000000	; Scanline 108
+      .byte #%00000000	; Scanline 107
+      .byte #%00000000	; Scanline 106
+      .byte #%00000000	; Scanline 105
+      .byte #%00000000	; Scanline 104
+      .byte #%00000000	; Scanline 103
+      .byte #%00000000	; Scanline 102
+      .byte #%00000000	; Scanline 101
+      .byte #%00000000	; Scanline 100
+      .byte #%00000000	; Scanline 99
+      .byte #%00000000	; Scanline 98
       .byte #%00000000	; Scanline 97
       .byte #%00000000	; Scanline 96
 
@@ -504,37 +588,37 @@ Screen_PF1
       .byte #%11111111	; Scanline 131
       .byte #%11111111	; Scanline 130
       .byte #%11111111	; Scanline 129
-      .byte #%11111111	; Scanline 128
-      .byte #%11111111	; Scanline 127
-      .byte #%11111111	; Scanline 126
-      .byte #%11111111	; Scanline 125
-      .byte #%11111111	; Scanline 124
-      .byte #%11111111	; Scanline 123
-      .byte #%11111111	; Scanline 122
-      .byte #%11111111	; Scanline 121
-      .byte #%11111111	; Scanline 120
-      .byte #%11111111	; Scanline 119
-      .byte #%11111111	; Scanline 118
-      .byte #%11111111	; Scanline 117
-      .byte #%11111111	; Scanline 116
-      .byte #%11111111	; Scanline 115
-      .byte #%11111111	; Scanline 114
-      .byte #%11111111	; Scanline 113
-      .byte #%11111111	; Scanline 112
-      .byte #%11111111	; Scanline 111
-      .byte #%11111111	; Scanline 110
-      .byte #%11111111	; Scanline 109
-      .byte #%11111111	; Scanline 108
-      .byte #%11101111	; Scanline 107
-      .byte #%11101111	; Scanline 106
-      .byte #%11101111	; Scanline 105
-      .byte #%01101110	; Scanline 104
-      .byte #%01101010	; Scanline 103
-      .byte #%01101010	; Scanline 102
-      .byte #%00101010	; Scanline 101
-      .byte #%00100000	; Scanline 100
-      .byte #%00100000	; Scanline 99
-      .byte #%00100000	; Scanline 98
+      .byte #%00111111	; Scanline 128
+      .byte #%00111111	; Scanline 127
+      .byte #%00111111	; Scanline 126
+      .byte #%00111111	; Scanline 125
+      .byte #%00111111	; Scanline 124
+      .byte #%00111111	; Scanline 123
+      .byte #%00110011	; Scanline 122
+      .byte #%00110011	; Scanline 121
+      .byte #%00110011	; Scanline 120
+      .byte #%00110011	; Scanline 119
+      .byte #%00110011	; Scanline 118
+      .byte #%00000011	; Scanline 117
+      .byte #%00000011	; Scanline 116
+      .byte #%00000011	; Scanline 115
+      .byte #%00000011	; Scanline 114
+      .byte #%00000000	; Scanline 113
+      .byte #%00000000	; Scanline 112
+      .byte #%00000000	; Scanline 111
+      .byte #%00000000	; Scanline 110
+      .byte #%00000000	; Scanline 109
+      .byte #%00000000	; Scanline 108
+      .byte #%00000000	; Scanline 107
+      .byte #%00000000	; Scanline 106
+      .byte #%00000000	; Scanline 105
+      .byte #%00000000	; Scanline 104
+      .byte #%00000000	; Scanline 103
+      .byte #%00000000	; Scanline 102
+      .byte #%00000000	; Scanline 101
+      .byte #%00000000	; Scanline 100
+      .byte #%00000000	; Scanline 99
+      .byte #%00000000	; Scanline 98
       .byte #%00000000	; Scanline 97
       .byte #%00000000	; Scanline 96
 
@@ -613,28 +697,28 @@ Screen_PF2
       .byte #%11111111	; Scanline 120
       .byte #%11111111	; Scanline 119
       .byte #%11111111	; Scanline 118
-      .byte #%11111111	; Scanline 117
-      .byte #%11111111	; Scanline 116
-      .byte #%11111111	; Scanline 115
-      .byte #%11111111	; Scanline 114
-      .byte #%11111111	; Scanline 113
-      .byte #%11111111	; Scanline 112
-      .byte #%11111111	; Scanline 111
-      .byte #%11111111	; Scanline 110
-      .byte #%11111111	; Scanline 109
-      .byte #%01111111	; Scanline 108
-      .byte #%01111111	; Scanline 107
-      .byte #%01111111	; Scanline 106
-      .byte #%01011101	; Scanline 105
-      .byte #%01010101	; Scanline 104
-      .byte #%01010101	; Scanline 103
-      .byte #%00010101	; Scanline 102
-      .byte #%00010001	; Scanline 101
-      .byte #%00010001	; Scanline 100
-      .byte #%00010001	; Scanline 99
-      .byte #%00010001	; Scanline 98
-      .byte #%00010001	; Scanline 97
-      .byte #%00010000	; Scanline 96
+      .byte #%00111111	; Scanline 117
+      .byte #%00111111	; Scanline 116
+      .byte #%00111111	; Scanline 115
+      .byte #%00110011	; Scanline 114
+      .byte #%00110011	; Scanline 113
+      .byte #%00110011	; Scanline 112
+      .byte #%00110011	; Scanline 111
+      .byte #%00110011	; Scanline 110
+      .byte #%00110000	; Scanline 109
+      .byte #%00110000	; Scanline 108
+      .byte #%00110000	; Scanline 107
+      .byte #%00110000	; Scanline 106
+      .byte #%00000000	; Scanline 105
+      .byte #%00000000	; Scanline 104
+      .byte #%00000000	; Scanline 103
+      .byte #%00000000	; Scanline 102
+      .byte #%00000000	; Scanline 101
+      .byte #%00000000	; Scanline 100
+      .byte #%00000000	; Scanline 99
+      .byte #%00000000	; Scanline 98
+      .byte #%00000000	; Scanline 97
+      .byte #%00000000	; Scanline 96
 
 BatSprite:
     .byte #%00000000
@@ -763,6 +847,17 @@ JColor:
     .byte #$0E;
     .byte #$C2;
     .byte #$C2;
+
+
+DigitsBitmap:
+     .byte $EE 
+     .byte $AA 
+     .byte $EE 
+     .byte $22 
+     .byte $EE 
+     
+
+Interrupt_Vectors 
 
     ORG $FFFA      ; move position
     .word Reset    ; NMI vector       
